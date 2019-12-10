bea976b22bddc1a15f83056622b5ffad941a49d1
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2019-12-09 22:08:24 -0800

    clean up code

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 const TeamPrompt = require("./lib/TeamPrompt");
-// const inquirer = require("inquirer"); 
-
-
 
 //prompt user for...
 const app = new TeamPrompt();
 app.start();
-// app.generateHTML();
-
-
-//email, id, and specific information based on their role with the company. 
-//For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
-
-//output file to team.html
